190e65b0c5fcb4a604a3cbd71e3bccbdac005ddf
fruitbob <1624596316@qq.com>
2018-07-31 21:16:46 +0800

    Create int.c

---
+++ int.c
@@ -0,0 +1,47 @@
+
+
+#include "bootpack.h"
+
+void init_pic(void)
+
+{
+	io_out8(PIC0_IMR,  0xff  ); 
+	io_out8(PIC1_IMR,  0xff  );
+
+	io_out8(PIC0_ICW1, 0x11  ); 
+	io_out8(PIC0_ICW2, 0x20  ); 
+	io_out8(PIC0_ICW3, 1 << 2); 
+	io_out8(PIC0_ICW4, 0x01  ); 
+
+	io_out8(PIC1_ICW1, 0x11  ); 
+	io_out8(PIC1_ICW2, 0x28  ); 
+	io_out8(PIC1_ICW3, 2     ); 
+	io_out8(PIC1_ICW4, 0x01  ); 
+
+	io_out8(PIC0_IMR,  0xfb  ); 
+	io_out8(PIC1_IMR,  0xff  ); 
+
+	return;
+}
+
+void inthandler21(int *esp)
+{
+	os_printfs((char *)0xa0000,320,100,50,4,"123");
+	for (;;) {
+		io_hlt();
+	}
+}
+
+void inthandler2c(int *esp)
+{
+	
+}
+
+void inthandler27(int *esp)
+{
+	io_out8(PIC0_OCW2, 0x67); 
+	return;
+}
+
+
+
